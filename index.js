b3e6cd3df7b1bcc6e455baae1fbd2d635333cb4f
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-04-29 14:21:51 -0500

    inital commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+const inquirer =require('inquirer');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: '@Gypsy1701!mas',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
